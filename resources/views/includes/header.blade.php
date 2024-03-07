1e1dbdf8ba797c0943b79da1d023cbb70392be10
leonardoolia <leonardooliaa@gmail.com>
2024-03-07 17:56:12 +0100

    added includes

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+        <div class="container">
+            <img  src="{{ asset('img/dc-logo.png') }}"   alt="Logo dc">
+
+            <ul>
+                <li><a href="{{ route('characters') }}">Characters</a></li>
+                <li><a href="{{ route('comics') }}">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </div>
+    </header>
